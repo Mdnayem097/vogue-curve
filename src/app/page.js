c5f99ebf9bb9d986c3f99f9afcf9aafd24e9355f
Mdnayem097 <mdneloy097@gmail.com>
2026-07-16 00:15:39 +0600

    add navbar, hero banner, and footer layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
+import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+// import {
+//   ArrowRight,
+//   Truck,
+//   RefreshCw,
+//   BadgeCheck,
+// } from "lucide-react";
 
 const HomePage = () => {
   return (
-    <div>
-      Hello World
+    <div className='sm:p-0 grid sm:grid-cols-1 lg:grid-cols-2 bg-[#F8FAFC] lg:p-15 gap-5'>
+      <section className="text-center lg:text-left order-2 lg:order-1">
+        <p className="inline-flex items-center gap-2 text-amber-600 font-semibold uppercase tracking-[0.2em] text-xs sm:text-sm mb-4">⚡ NEW COLLECTION 2026</p>
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold leading-tight text-slate-900">Elevate Your<br></br>Everyday Style</h1>
+        <p className='mt-5 text-slate-500 mb-10'>Discover premium local clothing designed for comfort, elegance, and timeless fashion. Explore curated collections crafted with premium fabrics for every occasion.</p>
+        <Link href='/shop' className='custom-btn'>Shop Collection <ArrowRight /></Link>
+      </section>
+      <section className="order-1 lg:order-2">
+        <Image alt='banner image' src="https://images.pexels.com/photos/9594675/pexels-photo-9594675.jpeg" width={700} height={700} className='rounded-xl'></Image>
+      </section>
     </div>
   );
 };
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,70 @@
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li className="hover:text-amber-600 transition-colors">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="hover:text-amber-600 transition-colors">
+                <Link href="/shop">Shop</Link>
+              </li>
+              <li className="hover:text-amber-600 transition-colors">
+                <Link href="/newArrivals">New Arrivals</Link>
+              </li>
+            </ul>
+          </div>
+          <Link href="/" className="text-xl px-2">
+            <span className="text-xl font-extrabold tracking-widest text-slate-900">
+              VOGUE<span className="text-amber-600">CURVE</span>
+            </span>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li className="hover:text-amber-600 transition-colors">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:text-amber-600 transition-colors">
+              <Link href="/shop">Shop</Link>
+            </li>
+            <li className="hover:text-amber-600 transition-colors">
+              <Link href="/newArrivals">New Arrivals</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end pr-3">
+          <button><ShoppingCart/></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
